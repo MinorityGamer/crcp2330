fcca5c601d3c9bf0d4b77a1b90a6279f8c792229
Samantha Guerrero <guerreros@smu.edu>
2018-03-06 11:18:01 -0600

    Test mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+
 // Put your code here.
+
+@2    //goes to the third slot (0,1,2)
+M=0	  //clears out the answer box
+
+@0     //goes to the first slot
+D=M    //if the slot is equal to zero
+@END
+D;JEQ	
+
+@1     //goes to the second slot 
+D=M    //if second slot is equal to zero 
+@END
+D;JEQ	
+
+
+(LOOP) //loop de loop
+@1	//at second slot 
+D=M	
+
+@2	//at third slot
+M=D+M	//RAM[2] holding number + previous val
+
+@3	//at fourth slot 
+M=M-1	//get first number and - 1
+
+D=M	
+@LOOP	
+D;JGT			 
+
+(END)
+@END
+0;JMP	//Loop to keep it from going off
