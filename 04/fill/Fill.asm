6c96b41d6182524a6cf666b002fd84c5581e0aaa
Samantha Guerrero <guerreros@smu.edu>
2018-03-06 11:54:19 -0600

    practice with output. Outline of fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -14,38 +14,22 @@
 // Put your code here.
 
 
-   (BLACKLOOP) 	   //Fills every pixel when key is pressed
-@24576		        //Keyboard register
-D=M		           //Keyboard register memory set to D
-@WHITELOOP	     //Goes to white loop if jump specification met
-D;JEQ		         //Jump, Goes to white loop if Keyboard memory register = 0
-@24575		       //@screen's last pixel
-D=M		           //@screen's last pixel memory set to D
-@WHITELOOP	     //Goes to white loop if jump specification met
-D;JLT		         //Jump, Goes to white loop if last pixel is filled, so not to exceed register amount
-@i		          //i, some register
-D=M		         //i memory set to D
-@16384		      //@screen's first pixel
-D=A+D		       //Register = @screen + i
-A=D		         //Register set
-M=-1		        //Register memory set to -1, fills all pixels in 16-bit register
-@i		        //i, some register
-M=M+1		       //Increments i's memory, which starts at zero
-@BLACKLOOP	   //Goes back to beginning of loop
-0;JMP		       //Null Jump, go back to black loop
+   /*   How many pixels? 
+   		1-button not pressed ->white, button pressed-> darken screen
+       if(if key input){
+           write black to every pixel.
+       }
+       if not 
+       		write white to every pixel 
 
-(WHITELOOP)	    //Removes every pixel when key is not pressed
-@24576		      //Keyboard Register
-D=M		           //Keyboard Register memory set to D
-@BLACKLOOP	     //Goes to black loop if jump specification met
-D;JGT		       //Jump, Goes to black loop if Keyboard memory register = 1
-@i		        //i, some register
-D=M		         //i memory set to D
-@16384		  //@screen's first pixel
-D=A+D		     //Register = @screen + i
-A=D		     //Register set
-M=0		       //Register memory set to 0, removes all filled pixels in 16-bit register
-@i		    //i, some register
-M=M-1		   //De-increments i's memory, which if the key has been pressed, has been raised to some number
-@WHITELOOP	//Goes back to beggining of loop
-0;JMP		     //Null Jump, go back to white loop
+   */
+
+   //-------------
+
+ (LOOP)
+ (BLACK)
+ M=-1
+ (WHITE)
+ M=0 
+ (END)
+
---
+++ 04/rectangle.asm
@@ -0,0 +1,40 @@
+//Program: Rectangle.asm 
+// draws a filled rectangle onto the screen 
+//for practice with input/output 
+
+@R0 
+D=M 
+@n 
+M=D //n = RAM[0] 
+
+@i 
+M=0 //i = 0 
+
+@SCREEN 
+D=A 
+@address 
+M=D //base address of the HACK screen 
+
+(LOOP)
+@i 
+D=M 
+@n 
+D=D-M 
+@END 
+D;JGT //if i>n goto END 
+
+@address 
+A=M 
+M=-1 //RAM[Address]=-1 (16 pxls)
+
+@i 
+M=M+1 
+@32 
+D=A
+@address
+M=D+M 
+@LOOP 
+0;JMP 
+
+(END)
+@END
