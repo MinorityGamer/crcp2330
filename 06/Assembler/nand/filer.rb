9010284eae7aa4acb70007db6231a35bd78da2e3
Samantha Guerrero <guerreros@smu.edu>
2018-05-07 13:29:43 -0500

    more planning

--- 06/Assembler/nand/filer.rb
+++ 06/Assembler/nand/filer.rb
@@ -5,21 +5,35 @@ class Parser
     @fileName = fileName
     @currChar = currChar
     @array_currInstructions = array_currInstructions
+  end
+  def createTable
+    #creates conversion table 
+  end
+  def readLabels
+    #reads through files and creates a table of labels 
+  def readIt
+    #reads through files and calls whatIs
+  def whatIs
+    #checks if it's a or c based off of the first bit 
+      #if it's a or c call a converter
+    #if it's a comment ignore it 
+  end
+  def convertA
     
   end
-  def readIt
-  end
-  def anyMore
-  end
-  def advance
-  end
-  def isComment
-  end
+  def convertC
+    #-Then need to break down each into it's parts 
+	#-what instruction is it? Dest? Comp? Jump?
+	#Ex: Of Parser and converter 
+#String c = parser.comp(); "M+1"
+#String d = parser.dest(); "D"
+#String j = parser.jump(); "JGT'
+#String biC = code.comp(c ); //"1110111"
+#String biD = code.dest(d); //"010"
+#String biJ = code.jump(j); //"001"
 
+String converted = "111"+biC+biD+biJ
+  end 
+    
 end
 
-#assembler opens file
-#reads first 'word' and puts temporarily in an variable
-#then looks at the most significant bit and if it's @
-#then it's an A instruction
-#if it's not then it's a c instruction
