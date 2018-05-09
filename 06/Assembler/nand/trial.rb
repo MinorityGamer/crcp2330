da0f1977e7317cb01c8a2522aeec76028734e2a8
Samantha Guerrero <guerreros@smu.edu>
2018-05-08 19:22:31 -0500

    have a table for conversion. Need to figure out how to connect the dots

--- 06/Assembler/nand/trial.rb
+++ 06/Assembler/nand/trial.rb
@@ -1,28 +1,31 @@
-#reads in a command. checks if it's an a or a c command.  
+#reads in a command. checks if it's an a or a c command.
 
 if ARGV.length != 1
-    puts "We need exactly one parameter. The name of a file."
+    puts "we need a file."
     exit;
 end
  
 filename = ARGV[0]
 puts "Going to open '#{filename}'"
  
-fh = open filename
+fi = open filename
  
-while (line = fh.gets) 
+while (line = fi.gets) 
    target_words = line.split (" ")
 
 target_words.each do |a_word|
     if a_word.start_with? '@'
       print "A instruction detected"
-    else print "C instruction detected"
+      #call A command and send in the a_word
+    else
+        print "C instruction detected"
         a_word.split(" ").each do |i|
-        puts i
+           #call command and sen each of the words split           
+           puts i
         end
     end
 end
 end
  
-fh.close
+fi.close
 
